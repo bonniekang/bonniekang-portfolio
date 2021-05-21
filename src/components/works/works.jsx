f3c70410067226694b048bdbafa99b1c135bbd68
jihye kang <jihye59kang@gmail.com>
2021-05-21 22:58:06 +0100

    projects page

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -6,42 +6,44 @@ export default function Works() {
             <div className="wrapper">
                 <div className="title">
                     <h1>PROJECTS</h1>
+                    <div className="border"></div>
                 </div>
                 <div className="projects">
                     <div className="project">
                         <div className="project-info">
                             <h2>The guild</h2>
                             <p>New type of task-based social platform for large group of friends or family</p>
+                            <div className="project-icon">
+                                <a href="https://theguild-lw.herokuapp.com" className="icon" target="_blank">live</a>
+                                <a href="https://github.com/tarumae/the-guild" className="icon" target="_blank">github</a>
                         </div>
-                        <div className="project-icon">
-                            <div className="icon">github</div>
-                            <div className="icon">view</div>
                         </div>
-                        <img src="" alt=""/>
+                        <div className="img-container"><img src="assets/guild-1.png" alt=""/></div>
                     </div>
 
                     <div className="project">
                         <div className="project-info">
                             <h2>Groundbnb</h2>
                             <p>AirBnb clone website having an allotment rental system</p>
+                            <div className="project-icon">
+                                <a href="https://groundbnb-tarumae.herokuapp.com" className="icon" target="_blank">live</a>
+                                <a href="https://github.com/tarumae/rails-groundbnb" className="icon" target="_blank">github</a>
                         </div>
-                        <div className="project-icon">
-                            <div className="icon">github</div>
-                            <div className="icon">view</div>
                         </div>
-                        <img src="" alt=""/>
+                        <div className="img-container"><img src="assets/groundbnb-1.png" alt=""/></div>
                     </div>
 
                     <div className="project">
                         <div className="project-info">
                             <h2>Watch List</h2>
                             <p>A CRUD web application for archiving movie selections</p>
+                            <div className="project-icon">
+                                <a href="https://movie-watch-list-app.herokuapp.com" className="icon" target="_blank">live</a>
+                                <a href="https://github.com/bonniekang/movie-watch-list" className="icon" target="_blank">github</a>
                         </div>
-                        <div className="project-icon">
-                            <div className="icon">github</div>
-                            <div className="icon">view</div>
                         </div>
-                        <img src="" alt=""/>
+
+                        <div className="img-container"><img src="assets/movie-1.png" alt=""/></div>
                     </div>
                 </div>
             </div>
