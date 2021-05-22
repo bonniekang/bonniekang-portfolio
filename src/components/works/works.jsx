64f3bebe8970a73585f4ab6aa38901c51c43c13f
jihye kang <jihye59kang@gmail.com>
2021-05-22 03:39:22 +0100

    add img scrollbar

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -18,7 +18,16 @@ export default function Works() {
                                 <a href="https://github.com/tarumae/the-guild" className="icon" target="_blank">github</a>
                         </div>
                         </div>
-                        <div className="img-container"><img src="assets/guild-1.png" alt=""/></div>
+                        <div className="img-container">
+                            <img src="assets/guild-1.png" alt=""/>
+                            <img src="assets/guild-2.png" alt=""/>
+                            <img src="assets/guild-3.png" alt=""/>
+                            <img src="assets/guild-4.png" alt=""/>
+                            <img src="assets/guild-5.png" alt=""/>
+                            <img src="assets/guild-6.png" alt=""/>
+                            <img src="assets/guild-7.png" alt=""/>
+                            <img src="assets/guild-8.png" alt=""/>
+                        </div>
                     </div>
 
                     <div className="project">
@@ -30,7 +39,13 @@ export default function Works() {
                                 <a href="https://github.com/tarumae/rails-groundbnb" className="icon" target="_blank">github</a>
                         </div>
                         </div>
-                        <div className="img-container"><img src="assets/groundbnb-1.png" alt=""/></div>
+                        <div className="img-container">
+                            <img src="assets/groundbnb-1.png" alt=""/>
+                            <img src="assets/groundbnb-2.png" alt=""/>
+                            <img src="assets/groundbnb-3.png" alt=""/>
+                            <img src="assets/groundbnb-4.png" alt=""/>
+                            <img src="assets/groundbnb-5.png" alt=""/>
+                        </div>
                     </div>
 
                     <div className="project">
@@ -43,7 +58,14 @@ export default function Works() {
                         </div>
                         </div>
 
-                        <div className="img-container"><img src="assets/movie-1.png" alt=""/></div>
+                        <div className="img-container">
+                            <img src="assets/movie-1.png" alt=""/>
+                            <img src="assets/movie-2.png" alt=""/>
+                            <img src="assets/movie-3.png" alt=""/>
+                            <img src="assets/movie-4.png" alt=""/>
+                            <img src="assets/movie-6.png" alt=""/>
+                            <img src="assets/movie-7.png" alt=""/>
+                        </div>
                     </div>
                 </div>
             </div>
