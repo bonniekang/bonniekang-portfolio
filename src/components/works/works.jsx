941d6525dcb2690bbbe5734013d35151561d47bc
jihye kang <jihye59kang@gmail.com>
2021-05-23 12:43:53 +0100

    add projects comments

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -19,13 +19,21 @@ export default function Works() {
                         </div>
                         </div>
                         <div className="img-container">
+                            <p>Homepage</p>
                             <img src="assets/guild-1.png" alt=""/>
+                            <p>User's guild list page</p>
                             <img src="assets/guild-2.png" alt=""/>
+                            <p>Create new guild</p>
                             <img src="assets/guild-3.png" alt=""/>
+                            <p>Guild page</p>
                             <img src="assets/guild-4.png" alt=""/>
+                            <p>Guild manage page</p>
                             <img src="assets/guild-5.png" alt=""/>
+                            <p>Invite new user</p>
                             <img src="assets/guild-6.png" alt=""/>
+                            <p>Create new milestone</p>
                             <img src="assets/guild-7.png" alt=""/>
+                            <p>Manage tasks for each milestone</p>
                             <img src="assets/guild-8.png" alt=""/>
                         </div>
                     </div>
@@ -40,10 +48,15 @@ export default function Works() {
                         </div>
                         </div>
                         <div className="img-container">
+                            <p>Homepage</p>
                             <img src="assets/groundbnb-1.png" alt=""/>
+                            <p>Search bar</p>
                             <img src="assets/groundbnb-2.png" alt=""/>
+                            <p>Allotment search page</p>
                             <img src="assets/groundbnb-3.png" alt=""/>
+                            <p>Allotment page and booking banner</p>
                             <img src="assets/groundbnb-4.png" alt=""/>
+                            <p>Booking manage page</p>
                             <img src="assets/groundbnb-5.png" alt=""/>
                         </div>
                     </div>
@@ -59,11 +72,17 @@ export default function Works() {
                         </div>
 
                         <div className="img-container">
+                            <p>Homepage</p>
                             <img src="assets/movie-1.png" alt=""/>
+                            <p>Show user's movie list</p>
                             <img src="assets/movie-2.png" alt=""/>
+                            <p>Movie page</p>
                             <img src="assets/movie-3.png" alt=""/>
+                            <p>Movie bookmarks</p>
                             <img src="assets/movie-4.png" alt=""/>
+                            <p>Create movie list</p>
                             <img src="assets/movie-6.png" alt=""/>
+                            <p>Add new movie to list</p>
                             <img src="assets/movie-7.png" alt=""/>
                         </div>
                     </div>
